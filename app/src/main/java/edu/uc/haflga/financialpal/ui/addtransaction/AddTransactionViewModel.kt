abe916001206158201caf8dbf7a290c076f895ab
ANagal5465 <53920484+ANagal5465@users.noreply.github.com>
2020-03-07 16:23:16 -0500

    Update AddTransactionViewModel.kt

--- app/src/main/java/edu/uc/haflga/financialpal/ui/addtransaction/AddTransactionViewModel.kt
+++ app/src/main/java/edu/uc/haflga/financialpal/ui/addtransaction/AddTransactionViewModel.kt
@@ -2,7 +2,8 @@ package edu.uc.haflga.financialpal.ui.addtransaction
 
 import androidx.lifecycle.ViewModel
 
-/** This will deliver the data to the AddTransactionFragement,
+/**
+ *  This will deliver the data to the AddTransactionFragement,
  *  aswell as hold the state of the data for late use.
  */
 class AddTransactionViewModel : ViewModel() {
